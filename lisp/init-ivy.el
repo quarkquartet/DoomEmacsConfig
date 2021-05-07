0f940066738afc7328188056f3d665d2806ba081
quarkquartet <hepisaacf@gmail.com>
2021-05-06 21:53:56 -0400

    icon

--- lisp/init-ivy.el
+++ lisp/init-ivy.el
@@ -2,26 +2,28 @@
 
 ;; Better experience with icons
 ;; Enable it before`ivy-rich-mode' for better performance
-;;(use-package! all-the-icons-ivy-rich
-;;  :if (icons-displayable-p)
-;;  :hook (ivy-mode . all-the-icons-ivy-rich-mode))
-;;
-;;;; More friendly display transformer for Ivy
-;;(use-package! ivy-rich
-;;  :hook ((counsel-projectile-mode . ivy-rich-mode) ; MUST after `counsel-projectile'
-;;         (ivy-rich-mode . (lambda ()
-;;                            "Use abbreviate in `ivy-rich-mode'."
-;;                            (setq ivy-virtual-abbreviate
-;;                                  (or (and ivy-rich-mode 'abbreviate) 'name)))))
-;;  :init
-;;  ;; For better performance
-;;  (setq ivy-rich-parse-remote-buffer nil))
 (use-package! all-the-icons-ivy-rich
   :ensure t
-  :init (all-the-icons-ivy-rich-mode 1))
+  :hook (ivy-mode . all-the-icons-ivy-rich-mode))
 
+;; More friendly display transformer for Ivy
 (use-package! ivy-rich
   :ensure t
-  :init (ivy-rich-mode 1))
+  :hook ((counsel-projectile-mode . ivy-rich-mode) ; MUST after `counsel-projectile'
+         (ivy-rich-mode . (lambda ()
+                            "Use abbreviate in `ivy-rich-mode'."
+                            (setq ivy-virtual-abbreviate
+                                  (or (and ivy-rich-mode 'abbreviate) 'name)))))
+  :init
+  ;; For better performance
+  (ivy-rich-mode 1)
+  (setq ivy-rich-parse-remote-buffer nil))
+;;(use-package! all-the-icons-ivy-rich
+;;  :ensure t
+;;  :init (all-the-icons-ivy-rich-mode 1))
+;;
+;;(use-package! ivy-rich
+;;  :ensure t
+;;  :init (ivy-rich-mode 1))
 
 (provide 'init-ivy)
