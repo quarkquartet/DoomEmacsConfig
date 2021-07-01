ea7ade4ab445f9058664da3522d5f5d88290d0b9
quarkquartet <hepisaacf@gmail.com>
2021-07-01 16:32:20 -0400

    theme solarized

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -8,10 +8,10 @@
   ;;(set-formatter! 'autopep8 "autopep8 -")
   (add-hook! 'before-save-hook 'py-isort-before-save)
 
-  (use-package! lsp-pyright
-    :config
-    (setq lsp-pyright-venv-path '(expand-file-name "~/envs/py37"))
-    )
+(use-package! lsp-pyright
+  :config
+  (setq lsp-pyright-venv-path '(expand-file-name "~/envs/py37"))
+  )
   )
 (defun my-python-checker ()
     (flycheck-select-checker 'python-flake8))
